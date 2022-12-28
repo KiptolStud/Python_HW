80aba96cfed43e8ebe3137017b24f3c50ac9d855
KiptolStud <Kiptol@yandex.ru>
2022-12-28 21:09:13 +0300

    add Hw4.1

--- Hw4/1Prog.py
+++ Hw4/1Prog.py
@@ -1,16 +1,13 @@
-#from decimal import Decimal, getcontext
-#getcontext().prec = 5
-#number = Decimal('9')
-#print(number.quantize(Decimal('0.0001')))
-
+#Вычислить число c заданной точностью d in
+#Enter a real number: 9
+#Enter the required accuracy '0.0001': 0.000001
+#out
+#9.000000
 
 chisl = int(input('Введите число: '))
-#from decimal import Decimal, getcontext
-#getcontext().prec = 5
-#number = Decimal(chisl)
-#print(number.quantize(Decimal('0.0001')))
+from decimal import Decimal, getcontext
+getcontext().prec = 7
+number = Decimal(chisl)
+print(number.quantize(Decimal('0.000001')))
 
-#from math import chisl
 
-d = int(input("Введите число для заданной точности числа Пи:\n"))
-print(f'число Пи с заданной точностью {d} равно {round(chisl, d)}')
